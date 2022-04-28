79e23dd957374915defeb1f83960f534fa699e7f
atsushiIMG <atusibba1014@yahoo.co.jp>
2022-04-28 14:07:48 +0900

    gitignore適用

--- jobs/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .. import views
-
-urlpatterns = [
-    path('vendors', views.ListView.as_view(), name='list'),
-    path('vendors/<int:pk>', views.DetailView.as_view(), name='detail'),
-    path('drinks', views.DrinksList.as_view(), name='drinkList'),
-    path('drinks/<int:pk>', views.DrinksDetail.as_view(), name='detail'),
-]
